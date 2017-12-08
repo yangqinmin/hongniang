6c676e4194660ab3d2253a80ca69ec17006e5c90
yangqinmin <964925536@qq.com>
2017-12-08 18:19:28 +0800

    新建项目

---
+++ app/src/main/java/com/example/hongniang/LoginActivity.java
@@ -0,0 +1,8 @@
+package com.example.hongniang;
+
+/**
+ * Created by yangq on 2017/12/8.
+ */
+
+public class LoginActivity {
+}
---
+++ app/src/main/java/com/example/hongniang/SecondFragment.java
@@ -0,0 +1,18 @@
+package com.example.hongniang;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+/**
+ * Created by dm on 16-3-29.
+ * 第一个页面
+ */
+public class SecondFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fg1, container, false);
+        return view;
+    }
+}
